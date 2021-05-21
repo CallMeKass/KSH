f7ae9ecbd5e5c802f17a24184c30b7484c5edb27
0xCA55 <cassie.holm97@gmail.com>
2021-05-20 23:00:36 -0500

    Add primative token seperation into list

--- src/program.cs
+++ src/program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace CJSH
 {
@@ -6,7 +7,36 @@ namespace CJSH
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("There's nothing here, sorry");
+            List<string> userInput = new List<string>();
+            while (true)
+            {
+                string prompt = "PLACEHOLDER: ";
+                Console.Write(prompt);
+                userInput = fetchTokens(Console.ReadLine());
+
+                foreach (string i in userInput)
+                {
+                    Console.WriteLine(i);
+                }
+
+                if (userInput[0] == "exit")
+                {
+                    Console.WriteLine("Goodbye");
+                    break;
+                }
+            }
+        }
+
+        // Converts a string into a list of string tokens
+        public static List<string> fetchTokens(string str)
+        {
+            List<string> output = new List<string>();
+            output.AddRange(str.Split(" "));
+
+            // Need to prevent splits inside quotes and after "\"
+
+            return output;
         }
     }   
 }
+
