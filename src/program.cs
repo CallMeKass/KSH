d84a4b157dd99e0c6d9a6abdb85737848667c9c3
0xCA55 <cassie.holm97@gmail.com>
2021-05-20 22:11:07 -0500

    Update .gitignore

---
+++ src/program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace CJSH
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("There's nothing here, sorry");
+        }
+    }   
+}
